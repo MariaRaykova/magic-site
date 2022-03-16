ddd748aedf4a2ed99a0978d1a95de9933f19afef
unknown <mimi.raykova@gmail.com>
2022-03-16 12:26:52 +0200

    add main page

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+
+
+
+const Footer = () => {
+    return (
+            <div>
+            <a target="_blank" href="https://icons8.com/icon/6wPGrsF25bmU/magic">Magic icon by Icons8</a>
+            </div>
+
+ 
+
+    )
+}
+export default Footer;
---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,27 @@
+import styles from "./index.module.css"
+
+import { Link } from "react-router-dom";
+
+const Icons = () => {
+    //https://codepen.io/sosuke/pen/Pjoqqp - Generate filter
+    return (
+        <div className={styles.icons}>
+        <span>
+        <img src="https://img.icons8.com/external-icongeek26-glyph-icongeek26/64/000000/external-magic-magic-and-fairy-tale-icongeek26-glyph-icongeek26-2.png"/>
+        </span>
+        <span>
+        <img src="https://img.icons8.com/external-outline-icons-pause-08/64/000000/external-magic-party2-outline-icons-pause-08.png"/>
+        </span>
+        <span>
+        <img src="https://img.icons8.com/external-out-line-pongsakorn-tan/64/000000/external-magic-nightlife-out-line-pongsakorn-tan.png"/>
+        </span>
+        <span>
+        <img src="https://img.icons8.com/external-others-phat-plus/64/000000/external-magic-design-thinking-outline-others-phat-plus.png"/>
+        </span>
+    </div>
+
+ 
+
+    )
+}
+export default Icons;
