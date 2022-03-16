057816e1afa25d7fe6c85dfbdb4efe4a72bd9385
teomihsav <teomihsav76@gmail.com>
2022-03-16 14:34:54 +0200

    Transition Removed

--- src/pages/GalleryPage/ContainerGallery/Image.tsx
+++ src/pages/GalleryPage/ContainerGallery/Image.tsx
@@ -9,19 +9,19 @@ import './ContainerGallery.css'
 
 const Image = (props: any) => {
     return <div>
-        <SwitchTransition mode='out-in'>
+        {/* <SwitchTransition mode='out-in'>
             <CSSTransition
                 key={props.index}
                 addEndListener={(node, done) => {
                     node.addEventListener("transitionend", done, false);
                   }}
                 classNames='fade'
-            >
+            > */}
                 <img className='img' 
                     src={props.path} alt=''
                 />
-            </CSSTransition>
-        </SwitchTransition>
+            {/* </CSSTransition>
+        </SwitchTransition> */}
     </div>
 }
 
