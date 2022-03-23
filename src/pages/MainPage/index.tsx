fc2a99e8bbfcbffd14ffbdc2239ebc85dfa14612
unknown <mimi.raykova@gmail.com>
2022-03-23 14:10:14 +0200

    clear unused rows

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,19 +2,16 @@ import ConsultationSection from "../../components/ConsultationSection";
 import IconsSection from "../../components/IconsSection";
 import ReserveSection from "../../components/ReserveSection";
 import SectionGallery from "../../components/SectionGallery";
-import Layout from "../Layout/Layout";
 import IntroSection from "../../components/IntroSection";
 
 const MainPage = () =>{
   return (
     <div>
-    {/* <Layout> */}
       <IntroSection />
       <IconsSection />
       <ReserveSection />
       <SectionGallery />
       <ConsultationSection />
-      {/* </Layout> */}
       </div>
   );
 }
